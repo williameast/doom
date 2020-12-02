853a9942fb2d8917083c2152bddaf5cc4b5130a2
William East <william.east@mail.mcgill.ca>
2020-12-02 21:30:12 +0100

    update

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/weast/org/todo.org" "/home/weast/org/journal/2020-11-20.org"))
+ '(package-selected-packages '(org-journal mpv transcribe geiser org-plus-contrib))
+ '(safe-local-variable-values '((transcription-mode . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
