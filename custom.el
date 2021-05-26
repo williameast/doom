cb62ec2c96c6b08219f3b5bceb6e83a225310e63
williameast <william.east@mail.mcgill.ca>
2021-05-26 19:20:05 +0200

    Updated snippets for ess-r-mode

--- custom.el
+++ custom.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(doct org-super-agenda ob-ipython pyenv-mode-auto lexic elpy org-timeline wordnut auto-dictionary powerthesaurus company-auctex auctex org-inline-pdf stock-tracker stock-ticker ytdl org-journal mpv transcribe geiser org-plus-contrib))
+   '(magit-todos doct org-super-agenda ob-ipython pyenv-mode-auto lexic elpy org-timeline wordnut auto-dictionary powerthesaurus company-auctex auctex org-inline-pdf stock-tracker stock-ticker ytdl org-journal mpv transcribe geiser org-plus-contrib))
  '(safe-local-variable-values '((transcription-mode . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:font #<font-spec nil nil monospace nil nil nil nil nil 14 nil nil nil nil>)))))
