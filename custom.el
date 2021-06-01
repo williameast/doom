9e2553fe8d2cbff095e7c9247f453469a29b578a
williameast <william.east@mail.mcgill.ca>
2021-06-01 10:53:05 +0200

    removed edit server

--- custom.el
+++ custom.el
@@ -1,15 +1,39 @@
+;;(custom-set-variables
+;; ;; custom-set-variables was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; '(org-agenda-files nil)
+;; '(package-selected-packages
+;;   '(magit-todos doct org-super-agenda ob-ipython pyenv-mode-auto lexic elpy org-timeline wordnut auto-dictionary powerthesaurus company-auctex auctex org-inline-pdf stock-tracker stock-ticker ytdl org-journal mpv transcribe geiser org-plus-contrib))
+;; '(safe-local-variable-values '((transcription-mode . t))))
+;;(custom-set-faces
+;; ;; custom-set-faces was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; '(fixed-pitch ((t (:font #<font-spec nil nil monospace nil nil nil nil nil 14 nil nil nil nil>)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
- '(package-selected-packages
-   '(magit-todos doct org-super-agenda ob-ipython pyenv-mode-auto lexic elpy org-timeline wordnut auto-dictionary powerthesaurus company-auctex auctex org-inline-pdf stock-tracker stock-ticker ytdl org-journal mpv transcribe geiser org-plus-contrib))
- '(safe-local-variable-values '((transcription-mode . t))))
+ '(ess-R-font-lock-keywords
+   '((ess-R-fl-keyword:keywords . t)
+     (ess-R-fl-keyword:constants . t)
+     (ess-R-fl-keyword:modifiers . t)
+     (ess-R-fl-keyword:fun-defs . t)
+     (ess-R-fl-keyword:assign-ops . t)
+     (ess-R-fl-keyword:%op% . t)
+     (ess-fl-keyword:fun-calls . t)
+     (ess-fl-keyword:numbers . t)
+     (ess-fl-keyword:operators . t)
+     (ess-fl-keyword:delimiters . t)
+     (ess-fl-keyword:= . t)
+     (ess-R-fl-keyword:F&T . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:font #<font-spec nil nil monospace nil nil nil nil nil 14 nil nil nil nil>)))))
+ )
