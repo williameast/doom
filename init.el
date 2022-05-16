a7f54f6c2085252d4bef28a71ab1746d73c85b25
William East <william.east@mail.mcgill.ca>
2022-05-16 14:24:24 +0200

    added rustlang

--- init.el
+++ init.el
@@ -163,7 +163,7 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        (sh +lsp)                  ; she sells {ba,z,fi}sh shells on the C xor
@@ -179,7 +179,7 @@
 
        :app
        ;;calendar
-       ;;irc               ; how neckbeards socialize
+       ;; irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
